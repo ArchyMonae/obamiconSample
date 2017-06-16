1d60b59dabb47c44938cecab652c4e58bbc291be
Carol Campbell <iamcarolcampbell@gmail.com>
2017-06-16 12:50:55 -0400

    Update obamicon.py

--- obamicon.py
+++ obamicon.py
@@ -1,37 +1,26 @@
+
 from PIL import Image
 
-# For recoloring.
+# RGB values for recoloring.
 darkBlue = (0, 51, 76)
 red = (217, 26, 33)
 lightBlue = (112, 150, 158)
 yellow = (252, 227, 166)
 
-# Load the image and turn the image into a list of tuples.
-my_image = Image.open("brooklyn.jpg")
-image_list = my_image.getdata()
-image_list = list(image_list)
+# Import image.
+my_image = Image.open("IMAGENAME") #change IMAGENAME to the path on your computer to the image you're using
+image_list = my_image.getdata() #each pixel is represented in the form (red value, green value, blue value, transparency). You don't need the fourth value.
+image_list = list(image_list) #Turns the sequence above into a list. The list can be iterated through in a loop.
 
+recolored = [] #list that will hold the pixel data for the new image.
+    
+#YOUR CODE to loop through the original list of pixels and build a new list based on intensity should go here.
+#loop through each pixel in the given list of image data
+    for pixel in image_data:
 
-# Check the intensity of each pixel, determine how to recolor it, and save it in a new list.
-recolored = []
-for pixel in image_list:
-
-    intensity = pixel[0] + pixel[1] + pixel[2]
-
-    if intensity < 182:
-         recolored.append(darkBlue)
-
-    elif intensity >= 182 and intensity < 364:
-        recolored.append(red)
-
-    elif intensity >= 364 and intensity < 546:
-        recolored.append(lightBlue)
-
-    elif intensity >=546:
-        recolored.append(yellow)
 
 # Create a new image using the recolored list. Display and save the image.
-new_image = Image.new("RGB", my_image.size)
-new_image.putdata(recolored)
-new_image.show()
-new_image.save("recolored.jpg", "jpeg")
+new_image = Image.new("RGB", my_image.size) #Creates a new image that will be the same size as the original image.
+new_image.putdata(recolored) #Adds the data from the recolored list to the image.
+new_image.show() #show the new image on the screen
+new_image.save("recolored.jpg", "jpeg") #save the new image as "recolored.jpg"
